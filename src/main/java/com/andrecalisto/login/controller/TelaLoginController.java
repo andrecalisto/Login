4cc76f42ec9a568f5740315864cd95d8c8c27652
Andre Calisto <andrecalisto@gmail.com>
2023-11-08 18:16:19 -0400

    inicializando o projeto login

---
+++ src/main/java/com/andrecalisto/login/controller/TelaLoginController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.andrecalisto.login.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author IFMT
+ */
+public class TelaLoginController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
