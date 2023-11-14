4c699714a534b9d60808f59ddba222cba909a3c9
Andre Calisto <andrecalisto@gmail.com>
2023-11-14 16:32:28 -0400

    criando a tela de login

--- src/main/java/com/andrecalisto/login/controller/TelaLoginController.java
+++ src/main/java/com/andrecalisto/login/controller/TelaLoginController.java
@@ -1,26 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package com.andrecalisto.login.controller;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
-/**
- * FXML Controller class
- *
- * @author IFMT
- */
-public class TelaLoginController implements Initializable {
+public class TelaLoginController {
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+    @FXML
+    private Hyperlink cadastreLink;
+
+    @FXML
+    private Button entrarButton;
+
+    @FXML
+    private PasswordField senhaField;
+
+    @FXML
+    private TextField usuarioField;
+
+    @FXML
+    void cadastrar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void login(ActionEvent event) {
+
+    }
+
 }
